4bddc52feab23ace53a9bb73b79cc8fbe18c860e
Hellguy <129449907+helguyuser1@users.noreply.github.com>
2024-06-15 18:49:48 -0300

    ParameterScaler.py

--- ParameterScaler.py
+++ ParameterScaler.py
@@ -1,334 +1,107 @@
 import requests
-
 import os
-
 from urllib.parse import urlparse
-
 import fade
-
 from colorama import Fore, Style
 
-
-
 def clear():
-
     if os.name == 'nt': 
-
         os.system('cls')
-
     else:  
-
         os.system('clear')
 
+def params(url):
+    parsedUrl = urlparse(url)
+    return bool(parsedUrl.query)
+
+def rmvd(urls):
+    filteredUrls = set()
+    folders = set()
+    for url in urls:
+        path = urlparse(url).path
+        folder = path.split('/')[-1]
+        if folder not in folders:
+            filteredUrls.add(url)
+            folders.add(folder)
+    return filteredUrls
+
+def find(siteUrl):
+    foundUrls = set()
+    try:
+        url = f"https://web.archive.org/cdx/search/cdx?url=*.{siteUrl}/*&output=txt&fl=original&collapse=urlkey&page=/"
+        response = requests.get(url)
+        if response.status_code == 200:
+            lines = response.text.splitlines()
+            for line in lines:
+                foundUrls.add(line)
+        else:
+            print(f"[X] Error: {response.status_code}")
+    except Exception as e:
+        print(f"[X] Error: {str(e)}")
+    return foundUrls
+
+def save(urls, siteUrl):
+    try:
+        fileName = siteUrl.replace('https://', '').replace('http://', '')  
+        if fileName.endswith('/'):
+            fileName = fileName[:-1]  
+        fileName += ".txt"
+
+        if os.path.exists(fileName):
+            number = 1
+            while True:
+                fileNameWithNumber = f"{fileName[:-4]} ({number}).txt"
+                if not os.path.exists(fileNameWithNumber):
+                    break
+                number += 1
+            fileName = fileNameWithNumber
+        
+        with open(fileName, 'w') as file:
+            for url in urls:
+                file.write(url + '\n')
+        print(f"[*] URLs saved in > '{fileName}'.")
+    except Exception as e:
+        print(f"[X] Error while saving URLs: {str(e)}")
+
 clear()
-
 text = """
-
     __  __________    __    ________  ____  __
-
    / / / / ____/ /   / /   / ____/ / / /\ \/ /
-
   / /_/ / __/ / /   / /   / / __/ / / /  \  / 
-
  / __  / /___/ /___/ /___/ /_/ / /_/ /   / /  
-
 /_/ /_/_____/_____/_____/\____/\____/   /_/   
-
                                               
-
     ✸  Tool made BY: Hellguy ! ✸
-
 """
 
-
-
-faded_text = fade.fire(text)
-
-print(faded_text)
-
-
+fadedText = fade.fire(text)
+print(fadedText)
 
 option = input("[1] - Only Parameters\n[2] - All URLs\n:> " + Fore.CYAN)
-
 print(Fore.RESET)
-
 if option == "1":
-
-    
-
-
-
-    def check_params(url):
-
-        parsed_url = urlparse(url)
-
-        if parsed_url.query:
-
-            return True
-
-        return False
-
-
-
-    def remove_duplicate_folders(urls):
-
-        filtered_urls = set()
-
-        folders = set()
-
-        for url in urls:
-
-            path = urlparse(url).path
-
-            folder = path.split('/')[-1]
-
-            if folder not in folders:
-
-                filtered_urls.add(url)
-
-                folders.add(folder)
-
-        return filtered_urls
-
-
-
-    def find_urls(site_url):
-
-        found_urls = set()
-
-        try:
-
-            url = f"https://web.archive.org/cdx/search/cdx?url=*.{site_url}/*&output=txt&fl=original&collapse=urlkey&page=/"
-
-            response = requests.get(url)
-
-            if response.status_code == 200:
-
-                lines = response.text.splitlines()
-
-                for line in lines:
-
-                    if line not in found_urls:
-
-                        found_urls.add(line)
-
-            else:
-
-                print(f"[X] Error: {response.status_code}")
-
-        except Exception as e:
-
-            print(f"[X] Error: {str(e)}")
-
-        return found_urls
-
-
-
-    def save_urls(urls, site_url):
-
-        try:
-
-            file_name = site_url.replace('https://', '').replace('http://', '')  
-
-            if file_name.endswith('/'):
-
-                file_name = file_name[:-1]  
-
-            file_name += ".txt"
-
-
-
-            if os.path.exists(file_name):
-
-                number = 1
-
-                while True:
-
-                    file_name_with_number = f"{file_name[:-4]} ({number}).txt"
-
-                    if not os.path.exists(file_name_with_number):
-
-                        break
-
-                    number += 1
-
-                file_name = file_name_with_number
-
-            
-
-            with open(file_name, 'w') as file:
-
-                for url in urls:
-
-                    file.write(url + '\n')
-
-            print(f"[*] URLs saved in > '{file_name}'.")
-
-        except Exception as e:
-
-            print(f"[X] Error while saving URLs: {str(e)}")
-
-
-
-    site_url = input("URL (https/http)\n:> " + Fore.CYAN)
-
-    urls = find_urls(site_url)
-
-    urls = remove_duplicate_folders(urls)
-
-
+    siteUrl = input("URL (https/http)\n:> " + Fore.CYAN)
+    urls = find(siteUrl)
+    urls = rmvd(urls)
 
     print("\n[+] URLs found with parameters:\n")
-
     for url in urls:
-
-        if check_params(url):
-
+        if params(url):
             print(Fore.GREEN + url + Fore.RESET)
 
-
-
-    save_urls(urls, site_url)
-
-    exit()
-
+    save(urls, siteUrl)
 elif option == "2":
-
-    def check_params(url):
-
-        parsed_url = urlparse(url)
-
-        if parsed_url.query:
-
-            return True
-
-        return False
-
-
-
-    def remove_duplicate_folders(urls):
-
-        filtered_urls = set()
-
-        folders = set()
-
-        for url in urls:
-
-            path = urlparse(url).path
-
-            folder = path.split('/')[-1]
-
-            if folder not in folders:
-
-                filtered_urls.add(url)
-
-                folders.add(folder)
-
-        return filtered_urls
-
-
-
-    def find_urls(site_url):
-
-        found_urls = set()
-
-        try:
-
-            url = f"https://web.archive.org/cdx/search/cdx?url=*.{site_url}/*&output=txt&fl=original&collapse=urlkey&page=/"
-
-            response = requests.get(url)
-
-            if response.status_code == 200:
-
-                lines = response.text.splitlines()
-
-                for line in lines:
-
-                    if line not in found_urls:
-
-                        found_urls.add(line)
-
-            else:
-
-                print(f"[X] Error: {response.status_code}")
-
-        except Exception as e:
-
-            print(f"[X] Error: {str(e)}")
-
-        return found_urls
-
-
-
-    def save_urls(urls, site_url):
-
-        try:
-
-            file_name = site_url.replace('https://', '').replace('http://', '')  
-
-            if file_name.endswith('/'):
-
-                file_name = file_name[:-1]  
-
-            file_name += ".txt"
-
-
-
-            if os.path.exists(file_name):
-
-                number = 1
-
-                while True:
-
-                    file_name_with_number = f"{file_name[:-4]} ({number}).txt"
-
-                    if not os.path.exists(file_name_with_number):
-
-                        break
-
-                    number += 1
-
-                file_name = file_name_with_number
-
-            
-
-            with open(file_name, 'w') as file:
-
-                for url in urls:
-
-                    file.write(url + '\n')
-
-            print(f"[*] URLs saved in > '{file_name}'.")
-
-        except Exception as e:
-
-            print(f"[X] Error while saving URLs: {str(e)}")
-
-
-
-    site_url = input("URL (https/http)\n:> " + Fore.CYAN)
-
-    urls = find_urls(site_url)
-
-    urls = remove_duplicate_folders(urls)
-
-
+    siteUrl = input("URL (https/http)\n:> " + Fore.CYAN)
+    urls = find(siteUrl)
+    urls = rmvd(urls)
 
     print("\n[+] URLs Found:\n")
-
     for url in urls:
-
-        if check_params(url):
-
+        if params(url):
             print(Fore.BLUE + "[!] Parameter found: " + url + Fore.RESET)
-
         else:
-
             print(Fore.GREEN + url + Fore.RESET)
 
+    save(urls, siteUrl)
 
-
-    save_urls(urls, site_url)
-
-    exit()
-
+exit()
